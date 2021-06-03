d4e38c0fc4128945cc59a3bf7ecd317b8427f3f6
Manishchandranaviri <85121579+Manishchandranaviri@users.noreply.github.com>
2021-06-03 19:24:51 +0530

    Update monte_carlo.c

--- monte_carlo.c
+++ monte_carlo.c
@@ -39,5 +39,19 @@ int main(void) {
   }
 }
 
-
+float mc_pi(int n ){
+   int incircle = 0;
+   
+  for ( int i=0; i<n ;i++ ){
+  float x = frandom();
+  float y= frandom();
+  float  d= x*x + y*y;
+  if ( d <= 1 ){
+   incircle++;
+   }
+  }
+  float pi = 4*(float)incircle/n;
+   return pi;   
+  
+  }
 
