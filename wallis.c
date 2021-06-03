b625dbac2d30b8cd45b78a5cc84be26cbdafd1d7
Manishchandranaviri <85121579+Manishchandranaviri@users.noreply.github.com>
2021-06-03 19:27:02 +0530

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -24,3 +24,11 @@ int main(void) {
   }
 }
 
+float wallis_pi(int count){
+    float pi = 1.0;
+    for (int i = 1; i < count; i++){
+        float num = 4.0 * i * i;
+        pi *= num / (num - 1);
+    }
+    return pi * 2;
+}
